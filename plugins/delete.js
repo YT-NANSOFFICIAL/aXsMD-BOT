349dcc10bac3d4c6c601b4d2e90bd4bd7239a407
A̐̈͢͢͢r̐̈͢͢͢d̐̈͢͢͢h̐̈͢͢͢i̐̈͢͢͢X̐̈͢͢͢s̐̈͢͢͢ <77874962+ArdhiXs@users.noreply.github.com>
2022-09-06 19:26:56 +0700

    Update delete.js

--- plugins/delete.js
+++ plugins/delete.js
@@ -1,12 +1,12 @@
 let handler = function (m) {
     if (!m.quoted) throw false
     let { chat, fromMe, id, isBaileys } = m.quoted
-    if (!isBaileys) throw 'Pesan tersebut bukan dikirim oleh bot!'
+    if (!isBaileys) throw '*Kamu bukan admin group, hanya bisa menghapus pesan yang dikirim oleh bot!*'
     conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: true, id: m.quoted.id, participant: m.quoted.sender } })
 
 }
 handler.help = ['del', 'delete']
-handler.tags = ['tools']
+handler.tags = ['main']
 
 handler.command = /^del(ete)?$/i
 handler.limit = true
