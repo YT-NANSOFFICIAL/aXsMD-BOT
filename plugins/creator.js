51221a0cbf1d0bc9016252a48caaee1806b367c0
A̐̈͢͢͢r̐̈͢͢͢d̐̈͢͢͢h̐̈͢͢͢i̐̈͢͢͢X̐̈͢͢͢s̐̈͢͢͢ <77874962+ArdhiXs@users.noreply.github.com>
2022-09-07 08:14:07 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,6 +1,29 @@
-function handler(m) {
-  const data = global.owner.filter(([id, isCreator]) => id && isCreator)
-  this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
+const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys')
+let handler = async (m, {conn}) => {
+const vcard = `BEGIN:VCARD
+VERSION:3.0
+N:;;;
+FN: ${nick}
+item.ORG: ${nick}
+item1.TEL;waid=${number}:${number}@s.whatsapp.net
+item1.X-ABLabel:owner selalu ada 
+item2.EMAIL;type=INTERNET: ${email}
+item2.X-ABLabel:📧Email
+item3.ADR:;;${kota};;;;
+item3.X-ABADR:ac
+item3.X-ABLabel:asal kota
+item4.URL: ${urlweb}
+item4.X-ABLabel:Website
+END:VCARD`
+const sentMsg  = await conn.sendMessage(
+    m.chat,
+    { 
+        contacts: { 
+            displayName: 'OWNER BOT', 
+            contacts: [{ vcard }]  
+        }
+    }
+)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
