8f1ce8bdd408c34adebf3f104a96571e1e137bd8
A̐̈͢͢͢r̐̈͢͢͢d̐̈͢͢͢h̐̈͢͢͢i̐̈͢͢͢X̐̈͢͢͢s̐̈͢͢͢ <77874962+ArdhiXs@users.noreply.github.com>
2022-09-08 13:08:17 +0700

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, args, usedPrefix, command, isOwner }) => {
 	if (!args[0]) throw `Link tiktoknya mana?\n\ncontoh:\n${usedPrefix}${command} https://vm.tiktok.com/ZGJAmhSrp/`
     tiktokdlv3(args[0]).then(r => {
     let video = r.video.no_watermark
-    conn.sendFile(m.chat, video, '', `*Link*: ${await shortlink(video)}\n\n_${wm}_`, m)
+    conn.sendFile(m.chat, video, '', `\n\n\n_${wm}_`, m)
     })
 }
 handler.help = ['tiktok'].map(v => v + ' <url>')
@@ -15,8 +15,7 @@ handler.command = /^(tt|tiktok|tik)$/i
 
 module.exports = handler
 
-
-async function shortlink(url) {
+/* async function shortlink(url) {
 isurl = /https?:\/\//.test(url)
 return isurl ? (await require('axios').get('http://ardhixs.c1.biz/txt.php?url='+encodeURIComponent(url))).data : ''
-}
+} */
