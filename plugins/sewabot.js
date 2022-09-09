206052b339b0d8a53210db49c4f38640a784406f
A̐̈͢͢͢r̐̈͢͢͢d̐̈͢͢͢h̐̈͢͢͢i̐̈͢͢͢X̐̈͢͢͢s̐̈͢͢͢ <77874962+ArdhiXs@users.noreply.github.com>
2022-09-09 08:33:02 +0700

    Update sewabot.js

--- plugins/sewabot.js
+++ plugins/sewabot.js
@@ -1,28 +1,27 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => {
   let pepe = 'https://telegra.ph/file/2e2eb7d093fec08fd00d4.jpg'
-  let baper = await fetch(pepe).then(a => a.buffer())
-
-  let aine = '6289654360447@s.whatsapp.net'
+  let recodekokbaper = await fetch(pepe).then(a => a.buffer())
+  let ardi = '6288294052009@s.whatsapp.net'
   let a = await conn.profilePictureUrl(conn.user.jid, 'image').catch((_) => "https://telegra.ph/file/24fa902ead26340f3df2c.png")
-  let b = await conn.profilePictureUrl(owner[0]+'@s.whatsapp.net', 'image').catch((_) => "https://telegra.ph/file/24fa902ead26340f3df2c.png")
-  let c = pickRandom([a, b])
-  let d = await fetch(c).then(a => a.buffer())
+  let r = await conn.profilePictureUrl(owner[0]+'@s.whatsapp.net', 'image').catch((_) => "https://telegra.ph/file/24fa902ead26340f3df2c.png")
+  let d = pickRandom([a, r])
+  let i = await fetch(d).then(a => a.buffer())
   let prepare = await require('@adiwajshing/baileys').generateWAMessageFromContent(m.key.remoteJid,{listMessage:{
   title: `${await conn.getName(conn.user.jid)}`,
   description: ` *• SEWA BOT & UP TO PREMIUM •*
         
 1. Grup / 30 Hari
-Rp. 15.000 Dana
+Rp. 10.000 Dana
 Rp. 15.000 Pulsa
 
 2. Premium / 30 Hari
-Rp. 10.000 Dana
+Rp. 5.000 Dana
 Rp. 10.000 Pulsa
 
 3. Premium + Grup / 30 Hari
-Rp. 20.000 Dana
-Rp. 20.000 Pulsa
+Rp. 10.000 Dana
+Rp. 15.000 Pulsa
 
 wa.me/${owner[0]}
 *Bukan Bot!!!*
@@ -35,10 +34,10 @@ wa.me/${owner[0]}
   title:'Klik untuk order',
   products:[{productId:'7487741964584083'}]}],
   headerImage: { productId: '7487741964584083',
-  jpegThumbnail: baper },
+  jpegThumbnail: recodekokbaper },
   businessOwnerJid: `6289654360447@s.whatsapp.net`
   },
-  footerText: 'https://aiinne.github.io',
+  footerText: wm,
   }},{})
   conn.relayMessage(prepare.key.remoteJid,prepare.message,{messageId:prepare.key.id})
   const data = global.owner.filter(([id, isCreator]) => id && isCreator)
@@ -47,7 +46,7 @@ wa.me/${owner[0]}
 }
 handler.help = ['sewa']
 handler.tags = ['main']
-handler.command = /^(sewa)$/i
+handler.command = /^sewa(bot)$/i
 
 module.exports = handler
 
